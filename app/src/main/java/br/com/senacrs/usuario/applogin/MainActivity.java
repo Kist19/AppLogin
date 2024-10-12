01ae0cbbe50a14820b9aaa7b9ac71bbdc8bd65e5
Kist19 <kistcross@gmail.com>
2024-10-11 22:52:35 -0300

    Arrumar

---
+++ app/src/main/java/br/com/senacrs/usuario/applogin/MainActivity.java
@@ -0,0 +1,40 @@
+package br.com.senacrs.usuario.applogin;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    Button btEntrar, btRegistar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        btEntrar =(Button) findViewById(R.id.btEntrar);
+        btRegistar =(Button) findViewById(R.id.btRegistar);
+
+        btRegistar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(i);
+            }
+        });
+
+        btRegistar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, RegistrarActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
